31f217730a4baf6dd6ba18c25616fd25f3efef08
billk2 <billkay2@mac.com>
2017-03-29 16:34:28 +0100

    tidy

--- monitor.ps1
+++ monitor.ps1
@@ -1,40 +1,34 @@
-Unregister-Event -SourceIdentifier FileChanged
-
-#Write-Host "monitor.ps1"
-# test git 2
-
 $dir = "C:\Users\IEUser\Saved Games\Frontier Developments\Elite Dangerous"
-#$dir = "F:\" #debug
-#$dir = "\\vboxsrv\journals"
 $filter = "*.log"
 
 $MutexName = "Global\ReadFile"
 $MutexWasCreated = $false;
+
 try {
-    $Mutex = [System.Threading.Mutex]::OpenExisting($MutexName);
+  $Mutex = [System.Threading.Mutex]::OpenExisting($MutexName);
 } catch {
-    $Mutex = New-Object System.Threading.Mutex($false,$MutexName,[ref]$MutexWasCreated);
+  $Mutex = New-Object System.Threading.Mutex($false, $MutexName, [ref]$MutexWasCreated);
 }
 
-$watcher = New-Object System.IO.FileSystemWatcher -Property @{Path = $dir;
-                                                              Filter = $filter;
-                                                              NotifyFilter = [System.IO.NotifyFilters]'FileName,LastWrite'}
+$watcher = New-Object System.IO.FileSystemWatcher -Property @{
+  Path = $dir;
+  Filter = $filter;
+  NotifyFilter = [System.IO.NotifyFilters]'FileName,LastWrite'
+}
 
+Register-ObjectEvent -InputObject $watcher -EventName Changed -SourceIdentifier FileCreated
 
-Register-ObjectEvent -InputObject $watcher -EventName Changed -SourceIdentifier FileCreated #-Action $CreatedActions
-
-while ($true){  
-    $events = @(Get-Event -SourceIdentifier FileCreated -ErrorAction SilentlyContinue )
-    foreach($event in $events) {
-        $Mutex.WaitOne() | Out-Null
-        try {
-            Get-Content $event.sourceEventArgs.FullPath -ReadCount 0 | Write-Host
-        } catch {
-            Write-Host $_
-        }
-        $Mutex.ReleaseMutex() | Out-Null
-
-        Remove-Event -EventIdentifier $event.EventIdentifier
+while ($true){
+  $events = @(Get-Event -SourceIdentifier FileCreated -ErrorAction SilentlyContinue)
+  foreach($event in $events) {
+    $Mutex.WaitOne() | Out-Null
+    try {
+      Get-Content $event.sourceEventArgs.FullPath -ReadCount 0 | Write-Host
+    } catch {
+      Write-Host $_
     }
-    $null = Wait-Event -SourceIdentifier FileCreated
+    $Mutex.ReleaseMutex() | Out-Null
+    Remove-Event -EventIdentifier $event.EventIdentifier
+  }
+  $null = Wait-Event -SourceIdentifier FileCreated
 }
