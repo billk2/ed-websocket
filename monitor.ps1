376099b802a5270c300a8fd273d5c6ef5b8448eb
billk2 <billkay2@mac.com>
2017-03-30 15:00:00 +0100

    remove debug

--- monitor.ps1
+++ monitor.ps1
@@ -2,56 +2,39 @@ param([string]$username = "test")
 
 Unregister-Event -SourceIdentifier FileChanged
 
-
 $dir = "C:\Users\$username\Saved Games\Frontier Developments\Elite Dangerous"
 $filter = "*.log"
 $global:FileChanged = $false
 $global:fileLengthLast = 0
-
-$temp = "$env:temp\ed-websocket.tmp"
-
-if (Test-Path $temp) {
-  Remove-Item $temp
-}
-# $tempFile = [io.path]::GetTempFileName()
 
 $Watcher = New-Object IO.FileSystemWatcher $dir, $filter -Property @{
   IncludeSubdirectories = $false;
   NotifyFilter = [System.IO.NotifyFilters]'FileName,LastWrite'
 }
 
-# Write-Host $dir
-
-
-
 Register-ObjectEvent $Watcher Changed -SourceIdentifier FileChanged -Action {
+
  $latestLog = $Event.SourceEventArgs.Name
  $global:fullPath = "$dir\$latestLog"
- #Write-Host $global:fullPath
  $lines = Get-Content $global:fullPath | Measure-Object -Line
-# Write-Host $lines.Lines
+
  if ($lines.Lines -ne $global:fileLengthLast) {
+
    if ($lines.Lines -lt $global:fileLengthLast) {
      $global:fileLengthLast = 0
    }
+
    $global:fileLengthChange = $lines.Lines - $global:fileLengthLast
- #  Write-Host "changed lines: $global:fileLengthChange"
    $global:fileLengthLast = $lines.Lines
-  # Write-Host "changed fileLengthLast: $global:fileLengthLast"
    $global:FileChanged = $true
   }
-  # } | Add-Content $temp
 }
 
 
 while ($true) {
 
-    while ($global:FileChanged -eq $true){
-      $global:FileChanged = $false
-   #   Write-Host $global:fullPath
-      Get-Content $global:fullPath -Tail $global:fileLengthChange
-    #  Write-Host "fileLengthChange: $global:fileLengthChange"
-      #Write-Host "debug"
-    }
-
+  while ($global:FileChanged -eq $true){
+    $global:FileChanged = $false
+    Get-Content $global:fullPath -Tail $global:fileLengthChange
+  }
 }
